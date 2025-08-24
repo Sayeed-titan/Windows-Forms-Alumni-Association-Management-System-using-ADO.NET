4d1e4ffed9856f8a7b363c1bc8ca57f6db8d0b6d
Sayeed-titan <sayeed1286265@gmail.com>
2025-08-25 01:33:55 +0600

    Add project files.

---
+++ AlumniAssociationApp/DatabaseHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AlumniAssociationApp
+{
+    public static class DatabaseHelper
+    {
+        private static string connectionString = @"Server=.\SQLEXPRESS;Database=AlumniDB;Trusted_Connection=True;";
+
+        public static DataTable GetData(string query)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+
+        public static void ExecuteCommand(string query, params SqlParameter[] parameters)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ SqlScripts/Database.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE AlumniDB;
+GO
+USE AlumniDB;
+GO
+
+CREATE TABLE Alumni (
+    AlumniID INT IDENTITY(1,1) PRIMARY KEY,
+    FullName NVARCHAR(100),
+    GraduationYear INT,
+    Email NVARCHAR(100),
+    Phone NVARCHAR(20)
+);
+
+CREATE TABLE Events (
+    EventID INT IDENTITY(1,1) PRIMARY KEY,
+    EventName NVARCHAR(100),
+    EventDate DATE,
+    Description NVARCHAR(500)
+);
+
+CREATE TABLE Attendance (
+    AttendanceID INT IDENTITY(1,1) PRIMARY KEY,
+    AlumniID INT FOREIGN KEY REFERENCES Alumni(AlumniID),
+    EventID INT FOREIGN KEY REFERENCES Events(EventID)
+);
